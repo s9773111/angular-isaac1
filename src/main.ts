e6ba8a92986877da488729c12eb030b03920452a
s9773111 <s9773111@gmail.com>
2023-04-14 09:34:14 +0800

    1120414_onSubmit

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,18 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  FormGroup,
+  FormBuilder,
+} from '@angular/forms';
 
 // 原本的元件
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   template: `
     <p>Hello from {{name}}!</p>
     <a target="_blank" href="https://angular.io/start">
@@ -30,19 +36,38 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
     <hr/>
 
+    <form [formGroup]="loginForm">
+      <label for="username">Username:</label>
+      <input type="text" id="username" formControlName="username">
+      <br/>
+      <label for="password">Password:</label>
+      <input type="password" id="password" formControlName="password">
+
+      <button type="submit">Login</button>
+    </form>
   `,
 })
 export class App {
   name = 'Angular Isaac Project';
   today: Date;
+  loginForm: FormGroup;
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: '',
+      password: '',
+    });
+  }
 
   ngOnInit(): void {
     this.today = new Date();
     setInterval(() => {
       this.today = new Date();
     }, 1000);
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
   }
 }
 
